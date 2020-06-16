0980bf98eb8dd7df86e9b541ad7f8590ae182aa6
Ripudaman Singh <ripu502@gmail.com>
2020-06-16 08:16:57 +0530

    working with one folder issue path

--- src/components/adminComponent/adminComponent.js
+++ src/components/adminComponent/adminComponent.js
@@ -1,26 +1,130 @@
 import React from 'react'
-import classes from './adminComponent.module.css';
+// import classes from './adminComponent.module.css';
+import Input from '../ui/Input/Input';
+import Button from '../ui/Button/BottonUI';
 
-const AdminComponent = () => {
-    return (
-        <div>
-            <h2>Admin login for BDCoE Member</h2>
-            <hr width="70%" />
-            <form action="/" method="post">
-                <div className={classes.Imgcontainer}>
-                    <img src="../Bdcoe/BDCoE.png" alt="bdcoe" className={classes.Avatar} />
-                </div>
-                <div className={classes.Container}>
-                    <label htmlFor="uname"><b>Username</b></label>
-                    <input type="text" placeholder="Enter Username" name="uname" required />
-                    <label htmlFor="psw"><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="psw" required />
-                    <button className={classes.Cancelbtn} type="submit">Login</button>
-                </div>
+class AdminComponent extends React.Component {
+
+    state = {
+        signIn: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
+            }
+        },
+        formIsValid: false,
+        loading: false
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedSignInForm = {
+            ...this.state.signIn
+        };
+        const updatedFormElement = {
+            ...updatedSignInForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.touched = true;
+        updatedSignInForm[inputIdentifier] = updatedFormElement;
+
+        let formIsValid = true;
+        for (let inputIdentifier in updatedSignInForm) {
+            formIsValid = updatedSignInForm[inputIdentifier].valid && formIsValid;
+        }
+        this.setState({ signIn: updatedSignInForm, formIsValid: formIsValid });
+    }
+
+
+    checkValidity(value, rules) {
+        let isValid = true;
+        if (!rules) {
+            return true;
+        }
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
+        }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|} ~-]+(?: \.[a - z0 - 9!#$ %& '*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        // if (rules.isNumeric) {
+        //     const pattern = /^\d+$/;
+        //     isValid = pattern.test(value) && isValid
+        // }
+
+        return isValid;
+    }
+
+
+    render() {
+        const formElementsArray = [];
+        for (let key in this.state.signIn) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.signIn[key]
+            });
+        }
+
+        let form = (
+            <form onSubmit={this.orderHandler}>
+                {formElementsArray.map(formElement => (
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+                ))}
+                <Button name='SignIn'></Button>
             </form>
-        </div>
+        );
 
-    )
+        return (
+            <div>
+                <h1> Admin signIn</h1>
+                {form}
+            </div>
+
+        )
+    }
 }
 
 export default AdminComponent;
