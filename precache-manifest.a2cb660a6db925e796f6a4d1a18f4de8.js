1fd66691527cfdfc24b7df24788d28a866479340
devanshsa5a <mail2devansh@gmail.com>
2020-08-12 00:00:59 +0530

    Updates

---
+++ precache-manifest.a2cb660a6db925e796f6a4d1a18f4de8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56bd59f85442f8bc377157022b111133",
+    "url": "/examportall/index.html"
+  },
+  {
+    "revision": "f1670a58c1c54d04974b",
+    "url": "/examportall/static/css/main.3bdb3567.chunk.css"
+  },
+  {
+    "revision": "abbe838bd4473296357e",
+    "url": "/examportall/static/js/2.c559d109.chunk.js"
+  },
+  {
+    "revision": "90f8df42b25649df7004ebf7bb2be3cf",
+    "url": "/examportall/static/js/2.c559d109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1670a58c1c54d04974b",
+    "url": "/examportall/static/js/main.d48bbd6c.chunk.js"
+  },
+  {
+    "revision": "21939577e5c200f5183f",
+    "url": "/examportall/static/js/runtime-main.2a6ac562.js"
+  },
+  {
+    "revision": "c5ad0581106cbcc8bb6e654d08362c0a",
+    "url": "/examportall/static/media/insta.c5ad0581.png"
+  },
+  {
+    "revision": "c6a29331f2dd104df47979265ff544be",
+    "url": "/examportall/static/media/logo.c6a29331.png"
+  },
+  {
+    "revision": "086936c9dcd8e5fcda43300dd2332957",
+    "url": "/examportall/static/media/logo1.086936c9.png"
+  }
+]);
