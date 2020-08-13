0f69e61b0b5595a7f0ee5cebc706230b5da02e3b
devanshsa5a <mail2devansh@gmail.com>
2020-08-13 18:51:29 +0530

    Updates

--- precache-manifest.64e0712e6d9790ba8228202e4d323a8b.js
+++ precache-manifest.64e0712e6d9790ba8228202e4d323a8b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "982be4c60606ba91e896164e4b679e0f",
+    "revision": "85e71a834ac1a915f9360ba94e6b9919",
     "url": "/examportall/index.html"
   },
   {
-    "revision": "dc818d801f238826f910",
-    "url": "/examportall/static/css/main.3bdb3567.chunk.css"
+    "revision": "9b7d3678e3c0513332f3",
+    "url": "/examportall/static/css/main.08bc85db.chunk.css"
   },
   {
-    "revision": "0030ffc981c6d879ada3",
-    "url": "/examportall/static/js/2.b806b713.chunk.js"
+    "revision": "f3fcabc1e4f6cd952282",
+    "url": "/examportall/static/js/2.bff3129f.chunk.js"
   },
   {
     "revision": "90f8df42b25649df7004ebf7bb2be3cf",
-    "url": "/examportall/static/js/2.b806b713.chunk.js.LICENSE.txt"
+    "url": "/examportall/static/js/2.bff3129f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dc818d801f238826f910",
-    "url": "/examportall/static/js/main.b9bdb084.chunk.js"
+    "revision": "9b7d3678e3c0513332f3",
+    "url": "/examportall/static/js/main.e08ab7d2.chunk.js"
   },
   {
     "revision": "21939577e5c200f5183f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/examportall/precache-manifest.84af9fe11d0e75eb83bf11cd7d7f6540.js"
+  "/examportall/precache-manifest.64e0712e6d9790ba8228202e4d323a8b.js"
 );
 
 self.addEventListener('message', (event) => {
